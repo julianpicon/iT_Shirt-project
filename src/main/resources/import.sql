0b9b4a1139b9061f629f29d7d1497956c402c2a6
Julián Picón <julian.picon@hotmail.com>
2017-04-04 02:27:42 -0500

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,6 +2,13 @@ INSERT INTO public.usua_rol(rol_id, rol_nombre, rol_sigla) VALUES (1, 'Administr
 INSERT INTO public.usua_rol(rol_id, rol_nombre, rol_sigla) VALUES (2, 'Artista', 'ARTIS');
 INSERT INTO public.usua_rol(rol_id, rol_nombre, rol_sigla) VALUES (3, 'Comprador', 'COMPR');
 
+
+INSERT INTO public.cata_tema(tema_id, tema_nombre) VALUES (1, 'Animales');
+INSERT INTO public.cata_tema(tema_id, tema_nombre) VALUES (2, 'Dibujos animados');
+INSERT INTO public.cata_tema(tema_id, tema_nombre) VALUES (3, 'Comics');
+INSERT INTO public.cata_tema(tema_id, tema_nombre) VALUES (4, 'Naturaleza');
+
+
 INSERT INTO public.usua_usuario(
 	usua_id, usua_nombre, usua_clave, usua_apellidos, usua_nombres, usua_email, usua_telefono, usua_genero, usua_direccion, usua_estado, rol_id)
 	VALUES (1, 'admin', 'admin', 'admin', 'admin', 'admin@itshirt.com', '111', 'M', 'Calle falsa 123', 'A', 1);
