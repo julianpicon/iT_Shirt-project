928f5a993978d5dd0bbeed9480102b446b55b29f
Julián Picón <julian.picon@hotmail.com>
2017-04-04 02:15:27 -0500

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,7 @@
 INSERT INTO public.usua_rol(rol_id, rol_nombre, rol_sigla) VALUES (1, 'Administrador', 'ADMIN');
 INSERT INTO public.usua_rol(rol_id, rol_nombre, rol_sigla) VALUES (2, 'Artista', 'ARTIS');
 INSERT INTO public.usua_rol(rol_id, rol_nombre, rol_sigla) VALUES (3, 'Comprador', 'COMPR');
+
+INSERT INTO public.usua_usuario(
+	usua_id, usua_nombre, usua_clave, usua_apellidos, usua_nombres, usua_email, usua_telefono, usua_genero, usua_direccion, usua_estado, rol_id)
+	VALUES (1, 'admin', 'admin', 'admin', 'admin', 'admin@itshirt.com', '111', 'M', 'Calle falsa 123', 'A', 1);
