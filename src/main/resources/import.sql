5983593f97df19a8bd1423163965d8a82df1370f
Julián Picón <julian.picon@hotmail.com>
2017-04-04 02:10:41 -0500

    Create import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.usua_rol(rol_id, rol_nombre, rol_sigla) VALUES (1, 'Administrador', 'ADMIN');
+INSERT INTO public.usua_rol(rol_id, rol_nombre, rol_sigla) VALUES (2, 'Artista', 'ARTIS');
+INSERT INTO public.usua_rol(rol_id, rol_nombre, rol_sigla) VALUES (3, 'Comprador', 'COMPR');
